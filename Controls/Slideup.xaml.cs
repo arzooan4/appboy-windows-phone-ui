93f65356e0da933cd0537c51636b25fb2ae11fa6
billmag <billmag687@gmail.com>
2014-01-04 15:20:37 +0200

    Initial release of Windows Phone UI code for Appboy's Windows Phone SDK.

---
+++ Controls/Slideup.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace AppboyUI.Phone.Controls {
+  public partial class Slideup : UserControl {
+    public Slideup() {
+      InitializeComponent();
+    }
+
+    public void SetText(string text) {
+      SlideupTextBlock.Text = text;      
+    }
+  }
+}
---
+++ Factories/SlideupFactory.cs
@@ -0,0 +1,12 @@
+﻿using AppboyPlatform.Phone.Managers;
+using System.Windows.Controls;
+
+namespace AppboyUI.Phone.Factories {
+  public class SlideupFactory : ISlideupFactory {
+    public UserControl GetSlideup(string message) {
+      var slideup = new Controls.Slideup();
+      slideup.SetText(message);
+      return slideup;
+    }
+  }
+}
